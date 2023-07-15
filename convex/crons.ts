9f262f150aec2ce87be76db1f439a8fda419c354
Thomas Ballinger <thomasballinger@gmail.com>
2023-07-15 15:38:40 -0700

    better (lol) ai players

--- convex/crons.ts
+++ convex/crons.ts
@@ -3,25 +3,55 @@ import { internalMutation } from "./_generated/server";
 import { api, internal } from "./_generated/api";
 import { createBall, getBalls, setName } from "./golf";
 
-export const cpuMove = internalMutation(async (ctx) => {
+export const cpuMove = internalMutation(
+  async (ctx, { identifier }: { identifier: string }) => {
+    const ball = (await ctx.db
+      .query("balls")
+      .withIndex("by_identifier")
+      .filter((q) => q.eq(q.field("identifier"), identifier))
+      .unique())!;
+    if (!ball.finished) {
+      ctx.scheduler.runAfter(0, api.golf.publishStroke, {
+        identifier,
+        angleInDegrees: Math.random() * 180,
+        mightiness: Math.random() * 20,
+      });
+      const delay = Math.floor(5000 + Math.random() * 5000);
+      ctx.scheduler.runAfter(delay, internal.crons.cpuMove, { identifier });
+    }
+  }
+);
+
+export const createCpu = internalMutation(async (ctx) => {
   const num = Math.ceil(Math.random() * 100);
   const identifier = "secretCPU-" + num;
   const balls = await getBalls(ctx, {});
   if (balls.length >= 4) return;
-  await createBall(ctx, { color: "red", identifier });
+  const cpuColor = [
+    "crimson",
+    "deeppink",
+    "hotpink",
+    "lightcoral",
+    "lightpink",
+    "magenta",
+    "mediumvioletred",
+    "orchid",
+    "pink",
+    "plum",
+    "salmon",
+    "tomato",
+  ];
+  const color = cpuColor[Math.floor(Math.random() * cpuColor.length)];
+  await createBall(ctx, { color, identifier });
   await setName(ctx, {
     name: "CPU " + num,
     identifier,
   });
-  for (const delay of [5000, 10000, 15000, 20000, 25000, 30000]) {
-    ctx.scheduler.runAfter(delay, api.golf.publishStroke, {
-      identifier,
-      angleInDegrees: Math.random() * 180,
-      mightiness: Math.random() * 20,
-    });
-  }
+  ctx.scheduler.runAfter(7000, internal.crons.cpuMove, {
+    identifier,
+  });
 });
 
 const crons = cronJobs();
-crons.interval("CPU move", { seconds: 60 }, internal.crons.cpuMove);
+crons.interval("CPU move", { seconds: 60 }, internal.crons.createCpu);
 export default crons;
