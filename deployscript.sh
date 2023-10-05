bc7c5524ab26079c818c2fd13f7f35c107d6791a
Tom Ballinger <tom@convex.dev>
2023-10-05 10:44:43 -0700

    deploy previews

---
+++ deployscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [[ $VERCEL_ENV == "preview"  ]] ; then 
+  export NEXT_PUBLIC_CONVEX_URL=$(npx convex preview $VERCEL_GIT_COMMIT_REF) && next build
+elif [[ $VERCEL_ENV == "prod" ]]; then  
+  next build && npx convex deploy
+else
+  # Command for dev if relevant
+  echo "No dev command"
+fi
